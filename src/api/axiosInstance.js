dd7a0e0eb6d4b2e73d540657af8e3aa93461fdbd
Ahmad Misbach <misbachrye@gmail.com>
2025-12-10 11:51:17 +0700

    update url server

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://2cd81fd9-4343-42cb-ab12-5919b1f3e80b-00-30b0krpryfp34.pike.replit.dev', 
+  baseURL: 'dwp-store-production.up.railway.app', 
   headers: {
     'Content-Type': 'application/json',
   },
